63707b2c7a444e970b7198112edf4adbd9f04fd3
Mark Billie <mbillie1@gmail.com>
2016-09-21 05:30:34 -0600

    parens, sass, web-mode

---
+++ personal/parens.el
@@ -0,0 +1,14 @@
+;;; package -- summary
+
+;;; Commentary:
+
+;;; Code:
+
+(defun goto-match-paren (arg)
+  "Go to the matching parenthesis if on parenthesis, otherwise insert %.
+vi style of % jumping to matching brace."
+  (interactive "p")
+  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+        (t (self-insert-command (or arg 1)))))
+
