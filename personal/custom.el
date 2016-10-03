443b234d21478912e2b8104d0cf4af7a33779de2
Mark Billie <mbillie1@gmail.com>
2016-10-03 05:21:58 -0600

    fix scss-mode

--- personal/custom.el
+++ personal/custom.el
@@ -7,10 +7,10 @@
    [default bold shadow italic underline bold bold-italic bold])
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-enabled-themes (quote (deeper-blue)))
+ '(custom-enabled-themes (quote (colonoscopy)))
  '(custom-safe-themes
    (quote
-    ("ac5584b12254623419499c3a7a5388031a29be85a15fdef9b94df2292d3e2cbb" "62408b3adcd05f887b6357e5bd9221652984a389e9b015f87bbc596aba62ba48" "726dd9a188747664fbbff1cd9ab3c29a3f690a7b861f6e6a1c64462b64b306de" "ea489f6710a3da0738e7dbdfc124df06a4e3ae82f191ce66c2af3e0a15e99b90" "2dd32048690787844d8cba601ed3dd8b2f419e9bd985898d0c3792671a05b96b" "9122dfb203945f6e84b0de66d11a97de6c9edf28b3b5db772472e4beccc6b3c5" "0c387e27a3dd040b33c6711ff92e13bd952369a788eee97e4e4ea2335ac5528f" "5cd0afd0ca01648e1fff95a7a7f8abec925bd654915153fb39ee8e72a8b56a1f" "f9574c9ede3f64d57b3aa9b9cef621d54e2e503f4d75d8613cbcc4ca1c962c21" "cdbd0a803de328a4986659d799659939d13ec01da1f482d838b68038c1bb35e8" "bffa9e9d8c1af5456a2a66957c5efe7dac080b5deca30ece7d41b96e966d3651" "db2ecce0600e3a5453532a89fc19b139664b4a3e7cbefce3aaf42b6d9b1d6214" "9c76a46072731fc4ba34c30ccb63636ffb573120028360369f623f1bc6a7cd99" "c3e6b52caa77cb09c049d3c973798bc64b5c43cc437d449eacf35b3e776bf85c" "5d61bf41bfda37fb1db418b7e41672a081247c4ee8fcf3226d00cd69c1af9fe8" "0aca3a26459bbb43a77f34bc22851c05c0a5d70d3230cbcdbda4fec20fef77e6" "70f5a47eb08fe7a4ccb88e2550d377ce085fedce81cf30c56e3077f95a2909f2" "8453c6ba2504874309bdfcda0a69236814cefb860a528eb978b5489422cb1791" "5a0eee1070a4fc64268f008a4c7abfda32d912118e080e18c3c865ef864d1bea" "419637b7a8c9cb43f273980f0c9879c0cbadace6b38efac0281e031772c84eb2" "040eab9c119c33f184e9fd21290ecea9691efc807a89c328e12a50d3a0fe60f8" "a2e7b508533d46b701ad3b055e7c708323fb110b6676a8be458a758dd8f24e27" "28ec8ccf6190f6a73812df9bc91df54ce1d6132f18b4c8fcc85d45298569eb53" "bcc6775934c9adf5f3bd1f428326ce0dcd34d743a92df48c128e6438b815b44f" "4af6fad34321a1ce23d8ab3486c662de122e8c6c1de97baed3aa4c10fe55e060" "fc0c179ce77997ecb6a7833310587131f319006ef2f630c5a1fec1a9307bff45" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "11636897679ca534f0dec6f5e3cb12f28bf217a527755f6b9e744bd240ed47e1" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "e80932ca56b0f109f8545576531d3fc79487ca35a9a9693b62bf30d6d08c9aaf" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default)))
+    ("d8f76414f8f2dcb045a37eb155bfaa2e1d17b6573ed43fb1d18b936febc7bbc2" "ac5584b12254623419499c3a7a5388031a29be85a15fdef9b94df2292d3e2cbb" "62408b3adcd05f887b6357e5bd9221652984a389e9b015f87bbc596aba62ba48" "726dd9a188747664fbbff1cd9ab3c29a3f690a7b861f6e6a1c64462b64b306de" "ea489f6710a3da0738e7dbdfc124df06a4e3ae82f191ce66c2af3e0a15e99b90" "2dd32048690787844d8cba601ed3dd8b2f419e9bd985898d0c3792671a05b96b" "9122dfb203945f6e84b0de66d11a97de6c9edf28b3b5db772472e4beccc6b3c5" "0c387e27a3dd040b33c6711ff92e13bd952369a788eee97e4e4ea2335ac5528f" "5cd0afd0ca01648e1fff95a7a7f8abec925bd654915153fb39ee8e72a8b56a1f" "f9574c9ede3f64d57b3aa9b9cef621d54e2e503f4d75d8613cbcc4ca1c962c21" "cdbd0a803de328a4986659d799659939d13ec01da1f482d838b68038c1bb35e8" "bffa9e9d8c1af5456a2a66957c5efe7dac080b5deca30ece7d41b96e966d3651" "db2ecce0600e3a5453532a89fc19b139664b4a3e7cbefce3aaf42b6d9b1d6214" "9c76a46072731fc4ba34c30ccb63636ffb573120028360369f623f1bc6a7cd99" "c3e6b52caa77cb09c049d3c973798bc64b5c43cc437d449eacf35b3e776bf85c" "5d61bf41bfda37fb1db418b7e41672a081247c4ee8fcf3226d00cd69c1af9fe8" "0aca3a26459bbb43a77f34bc22851c05c0a5d70d3230cbcdbda4fec20fef77e6" "70f5a47eb08fe7a4ccb88e2550d377ce085fedce81cf30c56e3077f95a2909f2" "8453c6ba2504874309bdfcda0a69236814cefb860a528eb978b5489422cb1791" "5a0eee1070a4fc64268f008a4c7abfda32d912118e080e18c3c865ef864d1bea" "419637b7a8c9cb43f273980f0c9879c0cbadace6b38efac0281e031772c84eb2" "040eab9c119c33f184e9fd21290ecea9691efc807a89c328e12a50d3a0fe60f8" "a2e7b508533d46b701ad3b055e7c708323fb110b6676a8be458a758dd8f24e27" "28ec8ccf6190f6a73812df9bc91df54ce1d6132f18b4c8fcc85d45298569eb53" "bcc6775934c9adf5f3bd1f428326ce0dcd34d743a92df48c128e6438b815b44f" "4af6fad34321a1ce23d8ab3486c662de122e8c6c1de97baed3aa4c10fe55e060" "fc0c179ce77997ecb6a7833310587131f319006ef2f630c5a1fec1a9307bff45" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "11636897679ca534f0dec6f5e3cb12f28bf217a527755f6b9e744bd240ed47e1" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "e80932ca56b0f109f8545576531d3fc79487ca35a9a9693b62bf30d6d08c9aaf" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default)))
  '(fci-rule-color "#383838")
  '(hl-paren-colors
    (quote
@@ -21,7 +21,7 @@
     ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(package-selected-packages
    (quote
-    (dakrone-theme colonoscopy-theme soft-charcoal-theme caroline-theme bliss-theme boron-theme busybee-theme flatland-black-theme gruvbox-theme ample-theme bubbleberry-theme geiser json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company smex ido-ubiquitous flx-ido vkill exec-path-from-shell zop-to-char zenburn-theme volatile-highlights undo-tree smartrep smartparens operate-on-number move-text magit projectile ov guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl easy-kill diminish diff-hl discover-my-major browse-kill-ring anzu ace-window ace-jump-buffer ace-jump-mode)))
+    (flycheck-swift spacegray-theme swift-mode geiser json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company smex ido-ubiquitous flx-ido vkill exec-path-from-shell zop-to-char zenburn-theme volatile-highlights undo-tree smartrep smartparens operate-on-number move-text magit projectile ov guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl easy-kill diminish diff-hl discover-my-major browse-kill-ring anzu ace-window ace-jump-buffer ace-jump-mode)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(powerline-color1 "#3d3d68")
  '(powerline-color2 "#292945")
@@ -52,13 +52,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-                                        ;old: "#876CA1"
- `(default ((t (:background "gray9")))) ;; or "black"
- '(font-lock-comment-face ((t (:foreground "#656361"))))
- '(font-lock-comment-delimiter-face ((t (:foreground "#656361"))))
- '(font-lock-string-face ((t (:foreground "#CB9B48"))))
+ '(default ((t (:background "gray13"))))
+ '(font-lock-comment-delimiter-face ((t (:foreground "#5E5E5E"))))
+ '(font-lock-comment-face ((t (:foreground "#5E5E5E"))))
+ '(font-lock-keyboard-face ((t (:foreground "#A26057"))))
  '(font-lock-keyword-face ((t (:foreground "#986F61"))))
- )
+ '(font-lock-string-face ((t (:foreground "#CB9B48")))))
 (global-set-key (kbd "C-%") 'goto-match-paren)
 (global-set-key (kbd "C-\"") 'toggle-quotes)
 (tool-bar-mode -1)
